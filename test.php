398aaca615b3d38c7e1bd70311e3d56c0be22fa0
Allen Rufolo <arufolo@cardinalsolutions.com>
2015-10-13 14:08:45 -0400

    more changes

--- test.php
+++ test.php
@@ -38,19 +38,23 @@ function InsertData($conn)
 {	
 echo "Inside The Insert Function <br>";
 
-    try
-    {
+    //try
+    //{
         $tsql = "INSERT INTO TextQueue (PhoneNumber, TextContent) VALUES ('+17047856193', 'This is a test insert' )";
         echo "TSQL Query: ".$tsql."<br>";
         $insertReview = sqlsrv_query($conn, $tsql);
 		echo "Insert Review: ".$insertReview."<br>";
 		
         if($insertReview == FALSE)
+		{
+			echo "Dying <br>";
             die(FormatErrors( sqlsrv_errors())); 
+		}
         while($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC))
-        {   
+        { 
             echo($row['ID']);
         }
+		/*
         sqlsrv_free_stmt($insertReview);
         sqlsrv_close($conn);
     }
