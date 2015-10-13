065864c3b06503857bc9f4effd41c52095476323
Allen Rufolo <arufolo@cardinalsolutions.com>
2015-10-13 10:29:28 -0400

    Re-Added PHP Code with new response

--- index.php
+++ index.php
@@ -4,5 +4,51 @@
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response><Message>Thank you for your reply!</Message></Response>'; //Place the desired response (if any) here
+echo '<Response><Message>Thank you for your reply.</Message></Response>'; //Place the desired response (if any) here
+ 
+InsertData({$_REQUEST['From']}, {$_REQUEST['Body']});
+ 
+ 
+function OpenConnection()
+{
+    try
+    {
+        $serverName = "tcp:j7luugiwb8.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"Texting_APP",
+            "Uid"=>"arufolo@cardinalsolutions.com@j7luugiwb8", "PWD"=>"P@ssw0rd1");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+
+function InsertData($from, $body)
+{
+    try
+    {
+        $conn = OpenConnection();
+
+        $tsql = "INSERT INTO Texting_APP.TextQueue (PhoneNumber, TextContent) VALUES ($from, $body)";
+        //Insert query
+        $insertReview = sqlsrv_query($conn, $tsql);
+        if($insertReview == FALSE)
+            die(FormatErrors( sqlsrv_errors()));
+        echo "Texting Queue Record Key inserted is :";   
+        while($row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC))
+        {   
+            echo($row['ID']);
+        }
+        sqlsrv_free_stmt($insertReview);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+
 ?>
