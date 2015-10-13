58e7c4de82181b19b5d6e715eae40999d2898de1
Allen Rufolo <arufolo@cardinalsolutions.com>
2015-10-13 10:26:37 -0400

    Added message to response

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response>Thank you for your reply!</Response>'; //Place the desired response (if any) here
+echo '<Response><Message>Thank you for your reply!</Message></Response>'; //Place the desired response (if any) here
 ?>
